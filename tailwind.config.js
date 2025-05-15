18e96b201ce07993419ba1c8a3f8b2582c6a3f0a
Sammyzain48 <samuelnwachukwu720@gmail.com>
2025-05-15 04:53:54 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {},
+    screen: {
+      'xs': {'max': '639px'},
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
